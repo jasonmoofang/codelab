907adbbec29dd2ada706ee2a7c062eb45b07e615
Lim Yuen Hoe <yuenhoe@hotmail.com>
2014-05-29 16:00:41 +0800

    Update shooting_rover.py

--- lesson_src/shooting_rover.py
+++ lesson_src/shooting_rover.py
@@ -3,21 +3,21 @@
 # open firing on anything that appears before it
 # suddenly!
 
-from ch.aplu.nxt import NxtContext, NxtRobot, Gear, TouchSensor, SensorPort, Tools, LightSensor
+from ch.aplu.nxt import NxtRobot, Motor, MotorPort, UltrasonicSensor, TouchSensor, SensorPort, Tools, LightSensor
 
 robot = NxtRobot()
 gunmotor = Motor(MotorPort.A)
 motor1 = Motor(MotorPort.B)
 motor2 = Motor(MotorPort.C)
-ts1 = TouchSensor(SensorPort.S1)
-ts2 = TouchSensor(SensorPort.S2)
-us = UltrasonicSensor(SensorPort.S3)
+ts1 = TouchSensor(SensorPort.S2)
+ts2 = TouchSensor(SensorPort.S3)
+us = UltrasonicSensor(SensorPort.S4)
 robot.addPart(gunmotor)
 robot.addPart(motor1)
 robot.addPart(motor2)
 robot.addPart(ts1)
 robot.addPart(ts2)
-robot.addPart(ls1)
+robot.addPart(us)
 
 
 def move_backward(m1, m2, t):
@@ -43,12 +43,13 @@ def start_forward(m1, m2):
     m2.forward()
   
 def stopfor(m1, m2, t):
-    m1.stop(t)
-    m2.stop(t)
+    m1.stop()
+    m2.stop()
+    Tools.delay(t)
 
-start_forward(m1, m2)
+start_forward(motor1, motor2)
 last_us_distance = -1
-while true:
+while True:
     t1 = ts1.isPressed()
     t2 = ts2.isPressed()
     if (t1 and t2):
@@ -57,20 +58,22 @@ while true:
         start_forward(motor1, motor2)
     else:
         if t1:
-            move_backward(motor1, motor2, 500)
-            turn(motor1, motor2, 400)
+            move_backward(motor1, motor2, 1000)
+            turn(motor2, motor1, 800)
             start_forward(motor1, motor2)
         elif t2:
-            move_backward(motor1, motor2, 500)
-            turn(motor2, motor1, 400)
+            move_backward(motor1, motor2, 1000)
+            turn(motor1, motor2, 800)
             start_forward(motor1, motor2)
 
     # if something appears close to sensor suddenly
-    if (last_us_distance != -1 && last_us_distance - us.getDistance() > 50):
+    if (last_us_distance != -1 and last_us_distance - us.getDistance() > 50):
         stopfor(motor1, motor2, 1000)
         # shoot it!
         gunmotor.forward()
-        Tools.delay(500)
+        Tools.delay(1000)
+        gunmotor.stop()
+        start_forward(motor1, motor2)
+
     last_us_distance = us.getDistance();
     Tools.delay(20)
-  
